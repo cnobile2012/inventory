36eaf8e012ccf8208c42dc9542953f3b9b939d8a
Carl J. Nobile <carl.nobile@gmail.com>
2015-04-04 16:33:49 -0400

    Added the projects app.

---
+++ inventory/projects/api/serializers.py
@@ -0,0 +1,25 @@
+#
+# inventory/projects/api/serializers.py
+#
+
+import logging
+
+from rest_framework import serializers
+
+from .models import Project
+
+
+log = logging.getLogger('projects.serializers')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    uri = serializers.HyperlinkedIdentityField(view_name='project-detail')
+    members = MemberSerializer(many=True, required=False)
+
+    class Meta:
+        model = Holiday
+        fields = ('pk', 'name', 'public', 'active', 'created', 'updated',
+                  'uri',)
+        exclude = ('creator', 'updater',)
+        read_only_fields = ('pk', 'created', 'updated',)
+        depth = 0
