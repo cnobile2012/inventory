075094bc8f04dd890a2948755665446b7f77052d
Carl J. Nobile <carl.nobile@gmail.com>
2015-09-05 19:31:31 -0400

    Fixed the URL pattern to allow negative PKs.

--- inventory/user_profiles/api/urls.py
+++ inventory/user_profiles/api/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns(
     'inventory.user_profiles.api.views',
     url(r'users/$', 'user_list', name='user-list'),
-    url(r'user/(?P<pk>\d+)/$', 'user_detail', name='user-detail'),
+    url(r'user/(?P<pk>[-\d]+)/$', 'user_detail', name='user-detail'),
     url(r'groups/$', 'group_list', name='group-list'),
     url(r'group/(?P<pk>\d+)/$', 'group_detail', name='group-detail'),
     url(r'user-profiles/$', 'user_profile_list', name="user-profile-list"),
