68857c1db3abc9a9718912d5d882c3d57b4a5e6e
Carl J. Nobile <carl.nobile@gmail.com>
2015-09-13 21:09:24 -0400

    Added an admin.py for Supplier.

---
+++ inventory/suppliers/admin.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# inventory/suppliers/admin.py
+#
+
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from dcolumn.common.admin_mixins import UserAdminMixin
+
+from .models import Supplier
+
+
+class SupplierAdmin(UserAdminMixin):
+    fieldsets = (
+        (None, {'fields': ('name', 'stype', 'address_01', 'address_02', 'city',
+                'region', 'postal_code', 'country', 'phone', 'fax', 'email',
+                'url',)}),
+        (_('Status'), {'classes': ('collapse',),
+                       'fields': ('active', 'creator', 'created', 'updater',
+                                  'updated',)}),
+        )
+    readonly_fields = ('creator', 'created', 'updater', 'updated',)
+    list_display = ('name', 'stype', 'phone', 'email', 'url', 'active',)
+    list_editable = ('stype', 'active',)
+    search_fields = ('country__country', 'city', 'region__region',
+                     'region__region_code',)
+    list_filter = ('stype', 'active',)
+    ordering = ('name',)
+
+admin.site.register(Supplier, SupplierAdmin)
