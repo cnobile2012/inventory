4a2f925376eb4a22b9fcbb1e90336f7cff2197c4
Carl J. Nobile <carl.nobile@gmail.com>
2015-10-11 18:59:10 -0400

    Changed the format of the root api objects.

--- inventory/common/api/views.py
+++ inventory/common/api/views.py
@@ -15,23 +15,23 @@ def api_root(request, format=None):
     """
     The root of all URIs found in this web service.
     """
+    # Buffer
     buff = OrderedDict()
-    accounts = OrderedDict()
-    oauth2 = OrderedDict()
-    projects = OrderedDict()
-    regions = OrderedDict()
-    suppliers = OrderedDict()
-
-    buff['Accounts'] = accounts
-    buff['OAuth2'] = oauth2
-    buff['Projects'] = projects
-    buff['Regions'] = regions
-    buff['Suppliers'] = suppliers
-
+    # Collection
+    collection = buff.setdefault('collection', OrderedDict())
+    collection['version'] = '1.0'
+    collection['href'] = reverse(
+        'api-root', request=request, format=format)
+    # Items
+    items = collection.setdefault('items', OrderedDict())
+    # Accounts
+    accounts = items.setdefault('accounts', OrderedDict())
     accounts['users'] = reverse(
         'user-list', request=request, format=format)
     accounts['user-groups'] = reverse(
         'group-list', request=request, format=format)
+    # Oauth2
+    oauth2 = items.setdefault('oauth2', OrderedDict())
     oauth2['access-token'] = reverse(
         'access-token-list', request=request, format=format)
     oauth2['applications'] = reverse(
@@ -40,12 +40,21 @@ def api_root(request, format=None):
         'grant-list', request=request, format=format)
     oauth2['refresh-token'] = reverse(
         'refresh-token-list', request=request, format=format)
+    # Projects
+    projects = items.setdefault('projects', OrderedDict())
     projects['projects'] = reverse(
         'project-list', request=request, format=format)
+    # Regions
+    regions = items.setdefault('regions', OrderedDict())
     regions['countries'] = reverse(
         'country-list', request=request, format=format)
     regions['regions'] = reverse(
         'region-list', request=request, format=format)
+    # Suppliers
+    suppliers = items.setdefault('suppliers', OrderedDict())
     suppliers['suppliers'] = reverse(
         'supplier-list', request=request, format=format)
+    # Queries
+    queries = collection.setdefault('queries', OrderedDict())
+
     return Response(buff)
