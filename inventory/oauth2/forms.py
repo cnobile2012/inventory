f5af4f1bb001bd2c134aa170e49391cfd9a4d352
Carl J. Nobile <carl.nobile@gmail.com>
2016-08-27 22:25:43 -0400

    Removed an unused app.

--- inventory/oauth2/forms.py
+++
@@ -1,7 +0,0 @@
-#
-# inventory/oauth2/forms.py
-#
-
-from django import forms
-from django.utils.translation import ugettext_lazy as _
-
--- inventory/oauth2/urls.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# inventory/oauth2/urls.py
-#
