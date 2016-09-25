10aa01fce37a7e07c07e6090fc5fc2d299fa89a4
Carl J. Nobile <carl.nobile@gmail.com>
2016-09-25 15:39:50 -0400

    Reorganized the permissions.

--- inventory/common/api/permissions.py
+++ inventory/common/api/permissions.py
@@ -7,122 +7,201 @@ import logging
 
 from django.contrib.auth import get_user_model
 
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
+
+from inventory.projects.models import Membership, Project
 
 
 log = logging.getLogger('api.common.permissions')
-User = get_user_model()
+UserModel = get_user_model()
 
 
-class IsAdminSuperUser(BasePermission):
+def get_user(request):
+    user = None
+
+    if hasattr(request, 'user'):
+        user = request.user
+
+    return user
+
+
+#
+# User based permissions
+#
+
+class IsAdminSuperUser(permissions.BasePermission):
     """
     Allows access only to admin super users.
     """
 
     def has_permission(self, request, view):
         result = False
+        user = get_user(request)
 
-        if request.user and request.user.is_superuser:
+        if user and user.is_superuser:
             result = True
 
         log.debug("IsAdminSuperUser: %s", result)
         return result
 
 
-class IsAdministrator(BasePermission):
+class IsAdministrator(permissions.BasePermission):
     """
     Allows access only to an administrator.
     """
 
     def has_permission(self, request, view):
         result = False
+        user = get_user(request)
 
-        if (hasattr(request, 'user') and
-            hasattr(request.user, 'role') and
-            request.user.role == User.ADMINISTRATOR):
+        if user and user.role == UserModel.ADMINISTRATOR:
             result = True
 
         log.debug("IsAdministrator: %s", result)
         return result
 
 
-class IsProjectManager(BasePermission):
-    """
-    Allows access only to project managers.
-    """
-
-    def has_permission(self, request, view):
-        result = False
-
-        if (hasattr(request, 'user') and
-            hasattr(request.user, 'role') and
-            request.user.role == User.PROJECT_MANAGER):
-            result = True
-
-        log.debug("IsProjectManager: %s", result)
-        return result
-
-
-class IsDefaultUser(BasePermission):
+class IsDefaultUser(permissions.BasePermission):
     """
     Allows access only to a logged in user with a profile.
     """
 
     def has_permission(self, request, view):
         result = False
+        user = get_user(request)
 
-        if (hasattr(request, 'user') and
-            hasattr(request.user, 'role') and
-            request.user.role == User.DEFAULT_USER):
+        if user and user.role == UserModel.DEFAULT_USER:
             result = True
 
         log.debug("IsDefaultUser: %s", result)
         return result
 
 
-class IsAnyUser(BasePermission):
+class IsAnyUser(permissions.BasePermission):
     """
     Allows any registered user.
     """
 
     def has_permission(self, request, view):
         result = False
+        user = get_user(request)
 
-        if (hasattr(request, 'user') and (
-            hasattr(request.user, 'role') and
-            request.user.role in (User.DEFAULT_USER, User.PROJECT_MANAGER,
-                                  User.ADMINISTRATOR,))):
+        # This permission is broken
+        if user and user.role in (UserModel.DEFAULT_USER,
+                                  UserModel.ADMINISTRATOR):
             result = True
 
         log.debug("IsAnyUser: %s", result)
         return result
 
 
-class IsReadOnly(BasePermission):
+#
+# Project based permissions
+#
+
+class BaseProjectPermission(permissions.BasePermission):
+    """
+    Handles project level permissions.
+    """
+
+    def has_project_permission(self, request, obj, level):
+        result = False
+        project = None
+        user = get_user(request)
+
+        if isinstance(Project, obj):
+            project = obj
+        elif hasattr(obj, 'project'):
+            project = obj.project
+
+        if not isinstance(level, (list, tuple)):
+            level = (level,)
+
+        if user and project and user.memberships.filter(
+            project=project, role__in=level):
+            result = True
+
+        return result
+
+
+class IsProjectOwner(BaseProjectPermission):
+    """
+    Allows access only to the project owner.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        result = self.has_project_permission(request, obj, Membership.OWNER)
+        log.debug("IsProjectOwner: %s", result)
+        return result
+
+
+class IsProjectManager(BaseProjectPermission):
+    """
+    Allows access only to the project managers.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        result = self.has_project_permission(request, obj,
+                                             Membership.PROJECT_MANAGER)
+        log.debug("IsProjectManager: %s", result)
+        return result
+
+
+class IsProjectDefaultUser(BaseProjectPermission):
+    """
+    Allows access only to a logged in user with a profile.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        result = self.has_project_permission(request, obj,
+                                             Membership.DEFAULT_USER)
+        log.debug("IsProjectDefaultUser: %s", result)
+        return result
+
+
+class IsAnyProjectUser(BaseProjectPermission):
+    """
+    Allows any registered user.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        result = self.has_project_permission(
+            request, obj, (UserModel.DEFAULT_USER,
+                           UserModel.PROJECT_MANAGER,
+                           UserModel.ADMINISTRATOR))
+        log.debug("IsAnyProjectUser: %s", result)
+        return result
+
+
+#
+# Miscellaneous permissions
+#
+
+class IsReadOnly(permissions.BasePermission):
     """
     The request is authenticated as a user, or is a read-only request.
     """
-    SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS',)
 
     def has_permission(self, request, view):
         result = False
 
-        if request.method in self.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             result = True
 
         log.debug("IsReadOnly: %s", result)
         return result
 
 
-class IsUserActive(BasePermission):
+class IsUserActive(permissions.BasePermission):
     """
     The request is authenticated if user is active.
     """
 
     def has_permission(self, request, view):
         result = False
+        user = get_user(request)
 
-        if hasattr(request, 'user') and request.user.is_active:
+        if user and user.is_active:
             result = True
 
         log.debug("IsUserActive: %s", result)
