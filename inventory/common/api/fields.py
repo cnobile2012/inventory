bc82db408eea99a03ef717591f197ad157d85b11
Carl J. Nobile <carl.nobile@gmail.com>
2016-09-29 16:24:23 -0400

    This is temporary code until I figure out what to do with this issue.

--- inventory/common/api/fields.py
+++ inventory/common/api/fields.py
@@ -12,6 +12,7 @@ import logging
 from django.core.exceptions import ObjectDoesNotExist
 
 from rest_framework import serializers
+from rest_framework.fields import Field
 
 log = logging.getLogger('api.common.fields')
 
@@ -42,3 +43,30 @@ class HyperlinkedCustomIdentityField(serializers.HyperlinkedRelatedField):
             raise ObjectDoesNotExist()
 
         return obj
+
+    def get_url(self, obj, view_name, request, format):
+        """
+        Given an object, return the URL that hyperlinks to the object.
+
+        May raise a `NoReverseMatch` if the `view_name` and `lookup_field`
+        attributes are not configured to correctly match the URL conf.
+        """
+        ## # Unsaved objects will not yet have a valid URL.
+        ## if hasattr(obj, 'pk') and obj.pk in (None, ''):
+        ##     return None
+
+        ## lookup_value = getattr(obj, self.lookup_field)
+
+        ## value = view_kwargs[self.lookup_url_kwarg]
+        ## value = int(value) if value.isdigit() else value
+        ## obj = None
+
+        ## for result in self.queryset:
+        ##     if value == getattr(result, self.lookup_field, ''):
+        ##         obj = result
+        ##         break
+
+        ## return obj
+        log.error("obj: %s", obj)
+        super(HyperlinkedCustomIdentityField, self).get_url(
+            obj, view_name, request, format)
