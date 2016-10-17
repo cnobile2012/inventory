93e17a106757ee7c7819e95847f3b1556e8f0a9e
Carl J. Nobile <carl.nobile@gmail.com>
2016-10-17 19:48:25 -0400

    Renamed the test_suppliers_api.py file.

---
+++ inventory/suppliers/api/tests/test_suppliers_api.py
@@ -0,0 +1,253 @@
+# -*- coding: utf-8 -*-
+#
+# inventory/suppliers/api/tests/test_suppliers_api.py
+#
+
+from rest_framework.reverse import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+from inventory.common.api.tests.base_test import BaseTest
+from inventory.suppliers.models import Supplier
+from inventory.regions.models import Country, Subdivision
+
+
+class TestSuppliers(BaseTest):
+
+    def __init__(self, name):
+        super(TestSuppliers, self).__init__(name)
+        self.country = None
+        self.region = None
+
+    def setUp(self):
+        super(TestSuppliers, self).setUp()
+        self.country = Country.objects.create(country='United States',
+                                              code='US',
+                                              active=True)
+
+    def test_create_post_supplier(self):
+        """
+        Ensure we can create a new supplier.
+        """
+        self.skipTest("Temporarily skipped")
+        # Use API to create a supplier.
+        uri = reverse('supplier-list')
+        region_uri = reverse('region-detail', kwargs={'pk': self.region.pk})
+        country_uri = reverse('country-detail', kwargs={'pk': self.country.pk})
+        new_data = {'name': 'Company01', 'address_01': '000 Someplace Road',
+                    'city': 'Anywhere', 'region': region_uri,
+                    'postal_code': '55144-1000', 'country': country_uri,
+                    'phone': '1-888-364-3577', 'stype': 1,}
+        response = self.client.post(uri, new_data, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_201_CREATED,
+            self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg)
+        msg = "Response Data: {}".format(data)
+        self.assertEqual(data.get('name'), new_data.get('name'), msg)
+        # Get the same record through the API.
+        pk = data.get('id')
+        uri = reverse('supplier-detail', kwargs={'pk': pk})
+        response = self.client.get(uri, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_200_OK, self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg)
+        self.assertEqual(data.get('name'), new_data.get('name'), msg)
+
+    def test_get_supplier_with_no_permissions(self):
+        """
+        Test the supplier_list endpoint with no permissions. We don't use the
+        self.client created in the setUp method from the base class.
+        """
+        self.skipTest("Temporarily skipped")
+        supplier = self._create_supplier()
+        username = 'Normal_User'
+        password = '123456'
+        user, client = self._create_normal_user(username, password, login=False)
+        # Use API to get user list with unauthenticated user.
+        uri = reverse('supplier-list')
+        response = client.get(uri, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_401_UNAUTHORIZED,
+            self._clean_data(data))
+        self.assertEqual(
+            response.status_code, status.HTTP_401_UNAUTHORIZED, msg)
+        self.assertTrue('detail' in data, msg)
+
+    def test_create_supplier_post_token(self):
+        """
+        Test supplier with API with token. We don't use the self.client
+        created in the setUp method from the base class.
+        """
+        self.skipTest("Temporarily skipped")
+        # Create a non-logged in user, but one that has a valid token.
+        username = 'Normal_User'
+        password = '123456'
+        user, client = self._create_normal_user(username, password,
+                                                email='test@example.com')
+        app_name = 'Token Test'
+        data = self._make_app_token(
+            user, app_name, client, client_type='public',
+            grant_type='client_credentials')
+        # Use API to create a supplier.
+        uri = reverse('supplier-list')
+        region_uri = reverse('region-detail', kwargs={'pk': self.region.pk})
+        country_uri = reverse('country-detail', kwargs={'pk': self.country.pk})
+        new_data = {'name': 'Company01', 'address_01': '000 Someplace Road',
+                    'city': 'Anywhere', 'region': region_uri,
+                    'postal_code': '55144-1000', 'country': country_uri,
+                    'phone': '1-888-364-3577', 'stype': 1,}
+        response = client.post(uri, new_data, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_201_CREATED,
+            self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg)
+
+    def test_update_put_supplier(self):
+        self.skipTest("Temporarily skipped")
+        # Use API to create a supplier.
+        uri = reverse('supplier-list')
+        region_uri = reverse('region-detail', kwargs={'pk': self.region.pk})
+        country_uri = reverse('country-detail', kwargs={'pk': self.country.pk})
+        new_data = {'name': 'Company01', 'address_01': '000 Someplace Road',
+                    'city': 'Anywhere', 'region': region_uri,
+                    'postal_code': '55144-1000', 'country': country_uri,
+                    'phone': '1-888-364-3577', 'stype': 1,}
+        response = self.client.post(uri, new_data, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_201_CREATED,
+            self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg)
+        self.assertFalse(data.get('fax'), msg)
+        # Update record with PUT.
+        pk = data.get('id')
+        uri = reverse('supplier-detail', kwargs={'pk': pk})
+        new_data['fax'] = '1-000-000-0000'
+        response = self.client.put(uri, new_data, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_200_OK, self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg)
+        self.assertTrue(data.get('fax'), msg)
+        # Read record with GET.
+        response = self.client.get(uri, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_200_OK, self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg)
+        self.assertEqual(data.get('fax'), new_data.get('fax'), msg)
+
+    def test_update_patch_supplier(self):
+        self.skipTest("Temporarily skipped")
+        # Use API to create a supplier.
+        uri = reverse('supplier-list')
+        region_uri = reverse('region-detail', kwargs={'pk': self.region.pk})
+        country_uri = reverse('country-detail', kwargs={'pk': self.country.pk})
+        new_data = {'name': 'Company01', 'address_01': '000 Someplace Road',
+                    'city': 'Anywhere', 'region': region_uri,
+                    'postal_code': '55144-1000', 'country': country_uri,
+                    'phone': '1-888-364-3577', 'stype': 1,}
+        response = self.client.post(uri, new_data, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_201_CREATED,
+            self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg)
+        self.assertFalse(data.get('fax'), msg)
+        # Update record with PATCH.
+        pk = data.get('id')
+        uri = reverse('supplier-detail', kwargs={'pk': pk})
+        update_data = {'fax': '1-000-000-0000'}
+        response = self.client.patch(uri, update_data, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_200_OK, self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg)
+        self.assertTrue(data.get('fax'), msg)
+        # Read record with GET.
+        response = self.client.get(uri, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_200_OK, self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg)
+        self.assertEqual(data.get('name'), new_data.get('name'), msg)
+        self.assertTrue(data.get('fax'), msg)
+
+    def test_delete_supplier(self):
+        self.skipTest("Temporarily skipped")
+        # Use API to create a supplier.
+        uri = reverse('supplier-list')
+        region_uri = reverse('region-detail', kwargs={'pk': self.region.pk})
+        country_uri = reverse('country-detail', kwargs={'pk': self.country.pk})
+        new_data = {'name': 'Company01', 'address_01': '000 Someplace Road',
+                    'city': 'Anywhere', 'region': region_uri,
+                    'postal_code': '55144-1000', 'country': country_uri,
+                    'phone': '1-888-364-3577', 'stype': 1,}
+        response = self.client.post(uri, new_data, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_201_CREATED,
+            self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg)
+        # Delete the User.
+        pk = data.get('id')
+        uri = reverse('supplier-detail', kwargs={'pk': pk})
+        response = self.client.delete(uri, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_200_OK, self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg)
+        self.assertTrue(data is None, msg)
+        # Get the same record through the API.
+        response = self.client.get(uri, format='json')
+        code = response.status_code
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_404_NOT_FOUND,
+            self._clean_data(data))
+        self.assertEqual(code, status.HTTP_404_NOT_FOUND, msg)
+
+    def test_options_user(self):
+        self.skipTest("Temporarily skipped")
+        # Use API to create a supplier.
+        uri = reverse('supplier-list')
+        region_uri = reverse('region-detail', kwargs={'pk': self.region.pk})
+        country_uri = reverse('country-detail', kwargs={'pk': self.country.pk})
+        new_data = {'name': 'Company01', 'address_01': '000 Someplace Road',
+                    'city': 'Anywhere', 'region': region_uri,
+                    'postal_code': '55144-1000', 'country': country_uri,
+                    'phone': '1-888-364-3577', 'stype': 1,}
+        response = self.client.post(uri, new_data, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_201_CREATED,
+            self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg)
+        # Get the API list OPTIONS.
+        pk = data.get('id')
+        response = self.client.options(uri, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_200_OK, self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg)
+        self.assertEqual(data.get('name'), 'Supplier List', msg)
+        # Get the API detail OPTIONS.
+        uri = reverse('supplier-detail', kwargs={'pk': pk})
+        response = self.client.options(uri, format='json')
+        data = response.data
+        msg = "Response: {} should be {}, content: {}".format(
+            response.status_code, status.HTTP_200_OK, self._clean_data(data))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg)
+        self.assertEqual(data.get('name'), 'Supplier Detail', msg)
+
+    def _create_supplier(self):
+        new_data = {'name': 'Company01', 'address_01': '000 Someplace Road',
+                    'city': 'Anywhere', 'region': self.region,
+                    'postal_code': '55144-1000', 'country': self.country,
+                    'phone': '1-888-364-3577', 'stype': 1,
+                    'updater': self.user, 'creator': self.user}
+        return Supplier.objects.create(**new_data)
