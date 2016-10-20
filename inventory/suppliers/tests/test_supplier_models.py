bbb5b578b71069453384fd757f410ed666048b51
Carl J. Nobile <carl.nobile@gmail.com>
2016-10-20 01:40:47 -0400

    Added an empty test file for the suppliers models.

---
+++ inventory/suppliers/tests/test_supplier_models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# inventory/suppliers/tests/test_supplier_models.py
+#
+
+from django.core.exceptions import ValidationError
+
+from inventory.common.tests.base_tests import BaseTest
+
+from ..models import Project, Membership
+
+
+class TestSupplierModels(BaseTest):
+
+    def __init__(self, name):
+        super(TestSupplierModels, self).__init__(name)
+
+    def setUp(self):
+        super(TestSupplierModels, self).setUp()
+        self.inventory_type = self._create_inventory_type()
+        self.project = self._create_project(self.inventory_type)
+
